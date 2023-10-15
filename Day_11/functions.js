7a712b1428105e406069b45488f1254842089be4
Anuranj N <anuranj2002@gmail.com>
2023-10-15 22:49:35 +0530

    Day 11 of Learning JS

---
+++ Day_11/functions.js
@@ -0,0 +1,16 @@
+//JavaScript inbuilt functions
+console.log(arr)
+var arr=[12,43,25,36,76,98]
+
+
+var a=arr.push(13)
+console.log('pushed array',arr)
+
+var b=arr.pop()
+console.log('popped array',arr)
+
+var c=arr.shift()
+console.log('shifted array',arr)
+
+var d=arr.unshift(95)
+console.log('unshifted array',arr)
---
+++ Day_11/objects.js
@@ -0,0 +1,21 @@
+//Object within an object practicing
+var ironman = {
+    fname:'Tony',
+    lname:'Stark',
+    Age: 67,
+    Avenger: true,
+    adress: {
+        state:'Kerala',
+        city: {
+            Name: 'kannur',
+            Pin: 670001
+        }
+    }
+}
+
+console.log(ironman)
+console.log(ironman.adress.city.Name)
+console.log(ironman.Age=25)
+console.log(ironman)
+
+
