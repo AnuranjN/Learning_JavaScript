6b82f5c164ce37d4a9bd6327d11ed02e0510182c
Anuranj N <anuranj2002@gmail.com>
2023-10-20 22:45:05 +0530

    Day 16 of JS

---
+++ Day_16/call_back.js
@@ -0,0 +1,13 @@
+function printFirstname(firstname,cb){
+    console.log(firstname)
+    cb('Nirmal')
+}
+
+function printLastname(lastname){
+    console.log(lastname)
+}
+
+printFirstname('Anuranj',printLastname)
+
+
+
