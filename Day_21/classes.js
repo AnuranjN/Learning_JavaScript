d4a2bc561e049623b992b12036ce4c56b39e17e7
Anuranj N <anuranj2002@gmail.com>
2023-10-25 23:28:19 +0530

    Day 21 of JS

---
+++ Day_21/classes.js
@@ -0,0 +1,13 @@
+class Student{
+    constructor(_name,_age){
+        this.name=_name
+        this.age=_age
+    }
+
+    
+}
+let student1=new Student('Anuranj',22)
+let student2=new Student('Mirzan',36)
+
+console.log(student1)
+console.log(student2)
